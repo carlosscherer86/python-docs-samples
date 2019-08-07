f466e6571899ecad90c8c7f61ab8dfa25049bd86
cscherer <cscherer@motorola.com>
2019-08-07 13:05:59 -0300

    test

--- iap/example_gce_backend.py
+++ iap/example_gce_backend.py
@@ -49,7 +49,7 @@ def resolve_content_type(request):
     if contentType == 'image/png':
         return send_file(BytesIO(request.content), mimetype=contentType)
     if contentType =='text/css':
-        return request.text
+        return Response(request.text, mimetype=contentType)
     return request.headers['content-type']
 
 if __name__ == '__main__':
