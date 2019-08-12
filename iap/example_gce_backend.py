2a331d4789e1297cc3b9cfc0d02c5bb31d8e4439
cscherer <cscherer@motorola.com>
2019-08-12 08:47:50 -0300

    test

--- iap/example_gce_backend.py
+++ iap/example_gce_backend.py
@@ -16,6 +16,7 @@ app = flask.Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
+    return flask.request.content_type
     token = flask.request.args.get('token')
     if not has_authorization(token) :
         return 'Unauthorized request.'
