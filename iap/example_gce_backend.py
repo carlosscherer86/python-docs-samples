59e28528b18eb29dbbb91cf36395de82c372e244
cscherer <cscherer@motorola.com>
2019-08-14 11:33:09 -0300

    test

--- iap/example_gce_backend.py
+++ iap/example_gce_backend.py
@@ -1,15 +1,8 @@
 import platform
 import flask
-import validate_jwt
-import requests
 
 from authentication import has_authorization
-from io import BytesIO
-from flask import send_file
-from flask import Response
-
-URL = 'https://34.66.62.85:8080'
-GET_METHOD = 'GET'
+from request_handler import handle_request
 
 app = flask.Flask(__name__)
 
@@ -18,40 +11,12 @@ def route_request():
     token = flask.request.args.get('token')
     if not has_authorization(token) :
         return 'Unauthorized request.'
-    return handle_request('')    
+    return handle_request('', flask.request)    
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return handle_request(path)
-    
-def handle_request(path):
-    if flask.request.method == GET_METHOD:
-        return get_request(path)
-    return ''
-
-def get_request(path):
-    params = get_parms()
-    r = requests.get(URL +'/'+ path + params, verify=False)
-    return resolve_content_type(r)
-
-def get_parms():
-    params = '';
-    if len(flask.request.args) > 0:
-        params = '?'
-        for f in flask.request.args:
-            params = params + f + '=' + flask.request.args.get(f) + '&'
-    print(params)
-    return params
-
-def resolve_content_type(request):
-    try:
-        contentType = request.headers['content-type']
-        if contentType == 'image/png' or contentType == 'image/gif':
-            return send_file(BytesIO(request.content), mimetype=contentType)
-        return Response(request.text, mimetype=contentType)
-    except:
-        return request.content
+    return handle_request(path, flask.request)
 
 if __name__ == '__main__':
     app.run()
---
+++ iap/request_handler.py
@@ -0,0 +1,35 @@
+import requests
+from flask import send_file
+from flask import Response
+from io import BytesIO
+
+GET_METHOD = 'GET'
+URL = 'https://34.66.62.85:8080'
+
+def handle_request(path, request):
+    if request.method == GET_METHOD:
+        return get_request(path, request)
+    return ''
+
+def get_request(path, request):
+    params = get_parms(request)
+    r = requests.get(URL +'/'+ path + params, verify=False)
+    return resolve_content_type(r)
+
+def get_parms(request):
+    params = '';
+    if len(request.args) > 0:
+        params = '?'
+        for f in request.args:
+            params = params + f + '=' + request.args.get(f) + '&'
+    print(params)
+    return params
+
+def resolve_content_type(request):
+    try:
+        contentType = request.headers['content-type']
+        if contentType == 'image/png' or contentType == 'image/gif':
+            return send_file(BytesIO(request.content), mimetype=contentType)
+        return Response(request.text, mimetype=contentType)
+    except:
+        return request.content
