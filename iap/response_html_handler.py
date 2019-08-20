ea72b6956e2d71f47049bff77656ce0dba4a1370
cscherer <cscherer@motorola.com>
2019-08-20 13:12:43 -0300

    teste

--- iap/response_html_handler.py
+++ iap/response_html_handler.py
@@ -10,7 +10,6 @@ SEARCH_LINK_WITH_TOKEN = '<a href="/search?token={0}&'
 XML_SCRIPT = '%2Fxml%3F'
 XML_SCRIPT_WITH_TOKEN = '%2Fxml%3Ftoken%3D{0}%26'
 def add_token(response, token):
-    print(response)
     reponseText = response.replace(INPUT_DIV, INPUT_DIV + HIDDEN_INPUT.format(token))
     reponseText = reponseText.replace(HOME_LINK, HOME_LINK_WITH_TOKEN.format(token))
     reponseText = reponseText.replace(SEARCH_LINK, SEARCH_LINK_WITH_TOKEN.format(token))
